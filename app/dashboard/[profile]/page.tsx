420b363e200ad655d33ae039b0eeb93ba53174d2
David-bit986 <tanasescudavid@gmail.com>
2026-03-18 20:08:52 +0200

    add dynamic export for forced re-rendering in profile page

--- app/dashboard/[profile]/page.tsx
+++ app/dashboard/[profile]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useSession } from "@/lib/auth-client";
 import { useEffect, useState } from "react";
 import PostCard from "@/components/dashboard/post-card";
